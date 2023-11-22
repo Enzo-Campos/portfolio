bd36c36c83aa423af8dede274474a0a30e983105
Enzo-Campos <93542684+Enzo-Campos@users.noreply.github.com>
2023-11-21 23:32:34 -0300

    mobile section layout fixed

--- src/components/SectionHero.tsx
+++ src/components/SectionHero.tsx
@@ -5,12 +5,12 @@ import Image from "next/image"
 
 export function SectionHero() {
     return (
-        <div className="bg-img-bg-hero h-[100vh] w-[100vw] bg-no-repeat bg-cover flex items-center mx-auto">
-            <div className="mx-auto flex w-[75vw] justify-between flex-col text-center sm:flex-row sm:text-left">
+        <div className="bg-img-bg-hero h-[100vh] w-[100vw] bg-no-repeat bg-cover flex mx-auto">
+            <div className="mx-auto flex w-[80vw] flex-col text-center sm:flex-row sm:text-left pt-8">
                 <div className="align-middle items-center">
-                    <h2 className="text-[4.2rem] font-bold leading-[7rem] text-white">Hello World!</h2>
-                    <h2 className="text-[2.7rem] font-light text-white mt-[-20px] mb-[20px]">Conheça meu portifólio.</h2>
-                    <p className="text-white font-light text-[1rem] max-w-md">Seja bem-vindo ao meu site, espero que aqui encontre as informações que precisa para conhecer um pouco mais de mim e no meu trabalho!</p>
+                    <h2 className="text-[12vw] font-bold text-white">Hello World!</h2>
+                    <h2 className="text-[2rem] font-light text-white mb-[20px]">Conheça meu portifólio.</h2>
+                    <p className="text-white font-light text-[1rem]">Seja bem-vindo ao meu site, espero que aqui encontre as informações que precisa para conhecer um pouco mais de mim e no meu trabalho!</p>
                     <a href="/#sobre"><button className="text-white bg-primary-green px-9 py-3 rounded-md my-8">Sobre mim</button></a>
                 </div>
                 <div>
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 export function Header() {
     return (
         <header className='w-full h-20 bg-primary-gray text-white flex items-center border-b-[1.8px] border-opacity-gray'>
-            <div className='w-full max-w-[75vw] flex justify-between align-middle mx-auto'>
+            <div className='w-full max-w-[90vw] flex justify-between align-middle mx-auto'>
                <a href="https://api.whatsapp.com/send?phone=5513996595151&text=Olá,%20Enzo%20gostei%20do%20seu%20portifólio." target='_blank'><h1>Enzo Campos</h1></a>
                <ul className='flex justify-evenly w-[24rem]'>
                 <a href="/"><li>Home</li></a>
