f1122f8b3a184186f3e686e267c3dd3bf110b3f8
Enzo-Campos <93542684+Enzo-Campos@users.noreply.github.com>
2024-07-19 12:16:52 -0300

    Date modified

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -7,7 +7,7 @@ const AboutMe = () => {
     xl:max-w-[70vw] xl:m-auto xl:my-[30px]
     lg:max-w-[80vw] lg:m-auto lg:my-[30px]' id='sobre'>
         <h2 className='text-[1.5rem] font-light mb-[15px]' id='portifolio'><strong>Sobre mim</strong></h2>
-        <p className='text-[0.8rem] font-thin xl:text-[1rem]'>Olá, meu nome é Enzo, tenho 22 anos e sou um <strong>Desenvolvedor Web</strong> com dois anos de experiência. Minha trajetória profissional tem se concentrado principalmente em tecnologias como <strong>WordPress</strong> para o desenvolvimento ágil de sites. No entanto, estou atualmente direcionando meus esforços para me especializar em desenvolvimento utilizando tecnologias como <strong>React</strong> e <strong>Next.js</strong>.
+        <p className='text-[0.8rem] font-thin xl:text-[1rem]'>Olá, meu nome é Enzo, tenho 22 anos e sou um <strong>Desenvolvedor Web</strong> com mais de dois anos de experiência. Minha trajetória profissional tem se concentrado principalmente em tecnologias como <strong>WordPress</strong> para o desenvolvimento ágil de sites. No entanto, estou atualmente direcionando meus esforços para me especializar em desenvolvimento utilizando tecnologias como <strong>React</strong> e <strong>Next.js</strong>.
           Aliás, este site que você está vendo foi <strong>inteiramente desenvolvido por mim</strong>, utilizando essas tecnologias e <strong>TypeScript,</strong> juntamente com o framework <strong>Tailwind CSS.</strong><br></br><br></br>
 
           Estou prestes a concluir o tecnólogo em <strong>Análise e Desenvolvimento de Sistemas.</strong> Durante minha formação, tive a oportunidade de trabalhar com tecnologias como <strong>Java</strong> e <strong>C#</strong>, embora não sejam minha preferência para desenvolvimento, elas me proporcionaram uma sólida base em lógica e matemática da computação.<br></br><br></br>
@@ -19,7 +19,7 @@ const AboutMe = () => {
         <p className='text-[0.8rem] font-thin xl:text-[0.9rem]'>
           <strong>Desenvolvedor Web</strong><br></br>
           Iblanco Marketing e Comunicação<br></br>
-          abr de 2022 - o momento · 1 ano 8 meses<br></br><br></br>
+          abr de 2022 - o momento · 2 anos 3 meses<br></br><br></br>
           Desenvolvimento e manutenção de plataformas WordPress e Moodle. Gerenciamento de Banco de Dados MySQL. Responsável pela infraestrutura do servidor e e-mails.<br></br><br></br>
           <strong>Competências:</strong> WordPress · Otimização para mecanismos de busca (SEO) · Google Drive · PHP · MySQL · Moodle · PhpMyAdmin · JavaScript · VPS
                   </p>
