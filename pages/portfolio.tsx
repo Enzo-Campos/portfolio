8adab0a416ac242891924666e6250b0aec454a12
Enzo-Campos <93542684+Enzo-Campos@users.noreply.github.com>
2024-07-19 13:22:34 -0300

    File not found on portfolio.tsx fixed

--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -17,16 +17,6 @@ export default function Portfolio() {
     <div className={poppins.className}>
       <Header />
       <Breadcrumb />
-      <div className='xl:grid xl:grid-cols-3 xl:max-w-[70vw] xl:m-auto xl:gap-x-[20px]
-      lg:grid lg:grid-cols-2 lg:max-w-[20vw] lg:m-auto lg:gap-x-[20px]'>
-        <SiteBox title="Site Institucional - FisioClinic Santos" subtitle="Desenvolvimento em wordpress." imageAlias='fisioclinic' siteLink='https://fisioclinicsantos.com.br' />
-        <SiteBox title="Site Institucional - Smartway" subtitle="Desenvolvimento em wordpress." imageAlias='smartway' siteLink='https://smartwaycs.com.br' />
-        <SiteBox title="Site Institucional - Ao Pharmacêutico" subtitle="Desenvolvimento em wordpress." imageAlias='campinas' siteLink='https://campinas-aopharmaceutico.com.br' />
-        <SiteBox title="Site Institucional - Relive Club" subtitle="Desenvolvimento em wordpress." imageAlias='reliveclub' siteLink='https://reliveclub.com.br' />
-        <SiteBox title="Site Institucional - Bem Barato Farma" subtitle="Desenvolvimento em wordpress." imageAlias='bembarato' siteLink='https://bembaratofarma.com.br' />
-        <SiteBox title="Site Institucional - Velis Contabil" subtitle="Desenvolvimento em Wordpress." imageAlias='velis' siteLink='https://veliscontabil.com.br' />
-        <SiteBox title="Site Institucional - Coocerqui Drogaria" subtitle="Desenvolvimento em wordpress." imageAlias='coocerqui' siteLink='https://coocerquidrogaria.com.br' />
-      </div>
       <Footer />
     </div>
   )
