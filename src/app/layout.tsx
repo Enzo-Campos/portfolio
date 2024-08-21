541f38b8b70537fb42e11ea186e10010369c98cd
Enzo-Campos <93542684+Enzo-Campos@users.noreply.github.com>
2024-08-21 19:28:36 -0300

    @next/font error fixed and GTM Tag Added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,22 @@ export const metadata = {
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="pt-br">
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-WLC6D2X2');
+            `,
+          }}
+        />
+      </head>
       <body className={poppins.className}>
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WLC6D2X2"
+        height="0" width="0"></iframe></noscript>
         <Header />
           {children}
         <Footer />
