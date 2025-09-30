fb1be2617f673f9d008ed03b751fad2747b66bdc
Enzo Campos <93542684+Enzo-Campos@users.noreply.github.com>
2025-09-30 16:21:44 -0300

    Add files via upload

---
+++ pages/bezan.js
@@ -0,0 +1,15 @@
+export default function FigmaEmbed() {
+  return (
+    <div style={{ margin: 0, padding: 0, width: "100vw", height: "100vh" }}>
+      <iframe
+        src="https://embed.figma.com/proto/T0k79bVBMWohDYaHkASPL2/Natalia-Bezan?node-id=1-3&p=f&scaling=scale-down&content-scaling=fixed&page-id=0%3A1&embed-host=share"
+        allowFullScreen
+        style={{
+          border: "none",
+          width: "100%",
+          height: "100%"
+        }}
+      />
+    </div>
+  );
+}
